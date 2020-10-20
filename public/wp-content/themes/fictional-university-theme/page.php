cf51ca2d2569832c738ef36833edd0b9b086d457
Elan Rasabi <elan.rasabi@tinroofsoftware.com>
2020-10-20 18:28:49 -0400

    9c page.php for individual pages

---
+++ public/wp-content/themes/fictional-university-theme/page.php
@@ -0,0 +1,14 @@
+<?php 
+    while(have_posts()) {
+        /* ?: stops php so you can write html */
+        the_post(); ?>
+        <h2> Page Test </h2>
+        <h2>
+            <?php the_title(); ?>
+        </h2>
+        <?php the_content(); ?>
+        <hr>
+        <?php 
+        /*  ?php: to resume php */
+    }
+?>
