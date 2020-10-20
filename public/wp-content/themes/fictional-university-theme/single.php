c6053f640a7dcc0930a5a4bde616172e0c780756
Elan Rasabi <elan.rasabi@tinroofsoftware.com>
2020-10-20 18:28:26 -0400

    9b single.php for individual posts

---
+++ public/wp-content/themes/fictional-university-theme/single.php
@@ -0,0 +1,13 @@
+<?php 
+    while(have_posts()) {
+        /* ?: stops php so you can write html */
+        the_post(); ?>
+        <h2>
+            <?php the_title(); ?>
+        </h2>
+        <?php the_content(); ?>
+        <hr>
+        <?php 
+        /*  ?php: to resume php */
+    }
+?>
