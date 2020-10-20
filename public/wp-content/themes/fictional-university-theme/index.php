45a556fae5369d329324c7cd1afe07f269d8d160
Elan Rasabi <elan.rasabi@tinroofsoftware.com>
2020-10-20 18:02:07 -0400

    8 php arrays

--- public/wp-content/themes/fictional-university-theme/index.php
+++ public/wp-content/themes/fictional-university-theme/index.php
@@ -1,20 +1,9 @@
 <?php 
-    function greet($name, $color) {
-        echo "<p>Hi, I'm $name and my fav color is $color </p>";
+    $names = array('N1', 'N2', 'N3', 'N4');
+
+    $count = 0;
+    while ( $count < count($names) ) {
+        echo "<li>Hi, I'm $names[$count] </li>";
+        $count++;
     }
-
-    greet('John', 'Blue');
 ?>
-
-<h1>
-    <?php 
-        // bloginfo is predefined function
-        // lets us access site info defined in Settings tab of admin panel 
-        bloginfo('name'); 
-    ?>
-</h1>
-<p>
-    <?php 
-        bloginfo('description'); 
-    ?>
-</p>
