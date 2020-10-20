b67b426557e181db960b245eca1cb4adcbe57ab6
Elan Rasabi <elan.rasabi@tinroofsoftware.com>
2020-10-20 18:23:02 -0400

    9a loop through all posts

--- public/wp-content/themes/fictional-university-theme/index.php
+++ public/wp-content/themes/fictional-university-theme/index.php
@@ -1,9 +1,13 @@
 <?php 
-    $names = array('N1', 'N2', 'N3', 'N4');
-
-    $count = 0;
-    while ( $count < count($names) ) {
-        echo "<li>Hi, I'm $names[$count] </li>";
-        $count++;
+    while(have_posts()) {
+        /* ?: stops php so you can write html */
+        the_post(); ?>
+        <h2>
+            <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+        </h2>
+        <?php the_content(); ?>
+        <hr>
+        <?php 
+        /*  ?php: to resume php */
     }
 ?>
