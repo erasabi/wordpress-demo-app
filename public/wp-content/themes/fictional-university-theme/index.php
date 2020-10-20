3dc5439405419baecfada455bf3b6d97f5fe943d
Elan Rasabi <elan.rasabi@tinroofsoftware.com>
2020-10-20 17:53:32 -0400

    7 php functions + bloginfo()

--- public/wp-content/themes/fictional-university-theme/index.php
+++ public/wp-content/themes/fictional-university-theme/index.php
@@ -1,1 +1,20 @@
-this is our completely amazing custome theme
+<?php 
+    function greet($name, $color) {
+        echo "<p>Hi, I'm $name and my fav color is $color </p>";
+    }
+
+    greet('John', 'Blue');
+?>
+
+<h1>
+    <?php 
+        // bloginfo is predefined function
+        // lets us access site info defined in Settings tab of admin panel 
+        bloginfo('name'); 
+    ?>
+</h1>
+<p>
+    <?php 
+        bloginfo('description'); 
+    ?>
+</p>
