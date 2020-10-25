dae0ff4d046a18fcf08c97e3470ec2c5fe38c5cb
Elan Rasabi <elan.rasabi@tinroofsoftware.com>
2020-10-24 21:45:12 -0400

    12b: in functions.php, inject new fonts, scripts, and styles so they are added to wp app

--- public/wp-content/themes/fictional-university-theme/functions.php
+++ public/wp-content/themes/fictional-university-theme/functions.php
@@ -1,6 +1,12 @@
 <?php
 
 function university_files() {
+    // load custom JS
+    wp_enqueue_script('main-university-js', get_theme_file_uri('/js/scripts-bundled.js'), NULL, '1.0', true);
+
+    // load custom fonts/styles
+    wp_enqueue_style('custom-google-fonts', '//fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i');
+    wp_enqueue_style('font-awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
     // wp_enqueue_styles('<nickname for stylesheet>', <location pointing to style file>)
     wp_enqueue_style('university_main_styles', get_stylesheet_uri());
 }
