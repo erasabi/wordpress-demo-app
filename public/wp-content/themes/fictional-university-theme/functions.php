6ba1aed9374fb66072d6acc6a6933394cf1acf3d
Elan Rasabi <elan.rasabi@tinroofsoftware.com>
2020-10-25 14:59:11 -0400

    14 Interior page instead of index.php for everything

--- public/wp-content/themes/fictional-university-theme/functions.php
+++ public/wp-content/themes/fictional-university-theme/functions.php
@@ -15,8 +15,14 @@ function university_files() {
     // wp_enqueue_styles('<nickname for stylesheet>', <location pointing to style file>)
     wp_enqueue_style('university_main_styles', get_stylesheet_uri());
 }
+
 // add_action(<name of hook to use>, <name of function associated>) 
 // add_action: function that lets you tell WP what action to execute
 add_action('wp_enqueue_scripts', 'university_files');
 
-?>
+// lets tab show name as page title
+function university_features() {
+  add_theme_support('title-tag');
+}
+
+add_action('after_setup_theme', 'university_features');
